4e1071be05b9e6c12f27fc019bb12c02a134dc54
ruas <ricardoruas88@gmail.com>
2019-01-17 00:35:39 -0200

    novo chance

--- src/main/java/br/com/ml/MercardoLivreApplication.java
+++ src/main/java/br/com/ml/MercardoLivreApplication.java
@@ -2,10 +2,16 @@ package br.com.ml;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class MercardoLivreApplication {
+public class MercardoLivreApplication extends SpringBootServletInitializer {
+ 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(MercardoLivreApplication.class);
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(MercardoLivreApplication.class, args);
